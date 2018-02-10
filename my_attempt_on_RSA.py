940fd07cc8379d75069cc838b52be92cd0022c4f
Chaosruler <trunks_ishter@hotmail.com>
2018-02-11 01:42:22 +0200

    Updated RSA's algorithm on my implentation

--- my_attempt_on_RSA.py
+++ my_attempt_on_RSA.py
@@ -1,30 +1,31 @@
-import random
-from Crypto.Util.number import isPrime
-from rsa.prime import gcd
+import timeit
 from random import randint
 
+from rsa.prime import gcd
+from sympy import isprime
 
-def coprime(a,b):
-    return gcd(a,b) == 1
+
+def coprime(a, b):
+    return gcd(a, b) == 1
 
 
-def encrypt(message,e,n):
+def encrypt(message, e, n):
     i = 0
     msg = 1
-    while i<e:
+    while i < e:
         msg *= message
-        msg = msg%n
-        i = i +1
+        msg = msg % n
+        i = i + 1
     return msg
 
 
-def decrypt(message,d,n):
-    i=0
-    msg=1
-    while i<d:
+def decrypt(message, d, n):
+    i = 0
+    msg = 1
+    while i < d:
         msg *= message
-        msg = msg%n
-        i = i +1
+        msg = msg % n
+        i = i + 1
     return msg
 
 
@@ -34,46 +35,56 @@ def xgcd(b, n):
         q, b, n = b // n, n, b % n
         x0, x1 = x1, x0 - q * x1
         y0, y1 = y1, y0 - q * y1
-    return  b, x0, y0
+    return b, x0, y0
 
 
-minPrime = 0
-maxPrime = 25000
-prime_numbers = [i for i in range(minPrime,maxPrime) if isPrime(i)]
+def generate_big_prime(n):
+    found_prime = False
+    while not found_prime:
+        p = randint(2 ** (n - 1), 2 ** n)
+        if isprime(p):
+            return p
 
-p = random.choice(prime_numbers)
 
-q = random.choice(prime_numbers)
+bits = 2048
+start = timeit.default_timer()
+p = generate_big_prime(bits)
 
-n = p*q
+q = generate_big_prime(bits)
 
-phi_n = (p-1)*(q-1)
+n = p * q
+
+phi_n = (p - 1) * (q - 1)
 
 print("n: %d\n" % n)
 
-e = randint(2,phi_n-1)
+print("phi(n): %d\n" % phi_n)
+
+e = randint(2, phi_n - 1)
 while not coprime(phi_n, e):
-    e=randint(2,phi_n-1)
+    e = randint(2, phi_n - 1)
 
 print("e: %d\n" % e)
 
-b,x0,y0 = xgcd(phi_n,e)
+b, x0, y0 = xgcd(phi_n, e)
 
-d=y0
+d = y0
 
 print("d: %d\n" % d)
 
-print("phi(n): %d\n" % phi_n)
+stop = timeit.default_timer()
+print("Created RSA key within %f s" % (stop - start))
 
 m = 500
 
-encrypted = encrypt(m,e,n)
+start = timeit.default_timer()
+encrypted = encrypt(m, e, n)
+stop = timeit.default_timer()
+print("Encrypted within %f s" % (stop - start))
 
-decrypted = decrypt(encrypted,d,n)
+start = timeit.default_timer()
+decrypted = decrypt(encrypted, d, n)
+stop = timeit.default_timer()
+print("Decrypted within %f s" % (stop - start))
 
-print("Message was %d\nEncrypted was %d, Decrypted was %d\n" % (m,encrypted,decrypted))
-
-
-
-
-
+print("Message was %d\nEncrypted was %d, Decrypted was %d\n" % (m, encrypted, decrypted))
